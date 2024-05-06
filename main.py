8a03f3ec73764df81793c486376f9d590fb86f73
Attiq30 <attiqrehman130@gmail.com>
2024-05-07 00:07:27 +0500

    Create columns , images and description

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1 :
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Attiq Ur Rehman")
+    content = """
+    Hi, i am Attiq Ur Rehman
+    
+    
+    """
+    st.info(content)
